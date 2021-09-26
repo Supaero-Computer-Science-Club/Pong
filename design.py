e8d810348e1dfae8c68d06b46e460a943a29cae1
M4thieuB <91211461+M4thieuB@users.noreply.github.com>
2021-09-26 22:59:34 +0200

    Add files via upload

--- design.py
+++ design.py
@@ -21,6 +21,7 @@ class Balle(object):
     def __init__(self, radius, initial_velocity,alpha, ini_pos,color): # Initial_velocity est la norme
         self.radius = radius
         self.norm = initial_velocity
+        self.angle = alpha
         self.vel_x = self.norm*math.cos(alpha)
         self.vel_y = self.norm*math.sin(alpha)
         self.x = ini_pos[0]
@@ -32,12 +33,13 @@ class Balle(object):
 
 
 
-def update_window(window, P1, P2, ball):
+def update_window(window, P1, P2, ball, scoreJ1, scoreJ2):
     """ Ecran noir puis on redessine dessus"""
     window.fill((0,0,0))
     P1.draw(window)
     P2.draw(window)
     ball.draw(window)
+    afficher_message(window, 'Score:  {}:{}'.format(scoreJ1, scoreJ2), 40, (255, 255, 255), (650, 30))
     pg.display.update()
 
 def verif_contact(raquette, ball):
@@ -51,4 +53,20 @@ def distribution_reflexion(raquette, ball, angle_max):
     y = ball.y - raquette.y - ymax
     return angle_max*(y/ymax)**3
 
+def afficher_message(window, message,taille, couleur,coordonnee):
+    """:param: coordonne est le tuple de coordonnée du message à afficher"""
+    font = pg.font.SysFont('Lato', taille)
+    msg = font.render(message, True, couleur)
+    window.blit(msg,coordonnee)
 
+def debut_manche(window,P1,P2,ball,scoreJ1,scoreJ2,initial_velocity):
+    window.fill((0,0,0))
+    pg.display.update()
+    pg.time.delay(500)
+    P1.y = 400
+    P2.y = 400
+    ball.x, ball.y = (720,420)
+    ball.norm = initial_velocity
+    ball.vel_x = ball.norm*math.cos(ball.angle)
+    ball.vel_y = ball.norm * math.sin(ball.angle)
+    update_window(window, P1, P2, ball, scoreJ1, scoreJ2)
--- main.py
+++ main.py
@@ -4,19 +4,22 @@ import random
 import math
 
 pg.init()
-
+# FPS
 clock = pg.time.Clock()
 # Fenêtre
 window = pg.display.set_mode((1420,840))
 pg.display.set_caption("Pong")
-
+# Définition des variables
 run = True
+scoreJ1 = 0
+scoreJ2 = 0
+initial_velocity = 15
+pause = False
 
 J1 = design.Raquette(250, 50, 15, (50,400), (255,0,0))
 J2 = design.Raquette(250, 50, 15, (1320,400), (255,0,0))
-ball = design.Balle(25, 15, random.randrange(-50,50)*math.pi/180, (720,420), (0,255,0))
+ball = design.Balle(25, initial_velocity, random.randrange(-50,50)*math.pi/180, (720,420), (0,255,0))
 
-cpt = 0
 # Boucle infinie du jeu
 while run:
     clock.tick(40)
@@ -27,51 +30,76 @@ while run:
             run = False
 
 # Gestion des raquettes
-    keys = pg.key.get_pressed() #Liste de booléen donnnant l'état de toutes les touches du clavier,
+    keys = pg.key.get_pressed() #Liste de booléen donnnant l'état de toutes les touches du clavier, cette ligne met à jour l'état de cette liste
     # Index fait en indiquant la touche du clavier
-    if keys[pg.K_z] and (J1.y - J1.velocity) > 0:
-        J1.y -= J1.velocity
-    if keys[pg.K_s] and (J1.y + J1.height + J1.velocity) < 840:
-        J1.y += J1.velocity
-    if keys[pg.K_o] and (J2.y - J2.velocity) > 0:
-        J2.y -= J2.velocity
-    if keys[pg.K_l] and (J2.y + J2.height + J2.velocity) < 840:
-        J2.y += J2.velocity
+    # Menu pause
+    if keys[pg.K_p]:
+        pause = True
+    elif keys[pg.K_e]:
+        pause = False
+    # et au moment d'afficher
+    if pause:
+        window.fill((0, 0, 0))
+        font = pg.font.SysFont('Lato', 100)
+        msg = font.render('Pause', True, (255, 255, 255))
+        window.blit(msg, (640, 300))
+        pg.display.update()
+    else:
+        if keys[pg.K_z] and (J1.y - J1.velocity) > 0:
+            J1.y -= J1.velocity
+        if keys[pg.K_s] and (J1.y + J1.height + J1.velocity) < 840:
+            J1.y += J1.velocity
+        if keys[pg.K_o] and (J2.y - J2.velocity) > 0:
+            J2.y -= J2.velocity
+        if keys[pg.K_l] and (J2.y + J2.height + J2.velocity) < 840:
+            J2.y += J2.velocity
 
-# Gestion de la balle
-    # Rebond sur les bords de la fenêtre
-    if (ball.y - ball.radius + ball.vel_y) < 0 or (ball.y + ball.radius + ball.vel_y) > 840:
-        ball.vel_y *= -1
-    # Rebond sur les raquettes avec distribution d'angles (ca marche po)
-    # if design.verif_contact(J1, ball) and (ball.x - ball.radius + ball.vel_x) <= (J1.x + J1.width):
-    #     nouvel_angle = design.distribution_reflexion(J1, ball, 55)
-    #     print(nouvel_angle, 'J1')
-    #     ball.vel_x = ball.norm*math.cos(nouvel_angle)
-    #     ball.vel_y = ball.norm*math.sin(nouvel_angle)
-    #
-    # if design.verif_contact(J2, ball) and (ball.x + ball.radius + ball.vel_x) >= J2.x:
-    #     nouvel_angle = design.distribution_reflexion(J2, ball, 55)
-    #     print(nouvel_angle, 'J2')
-    #     ball.vel_x = -ball.norm * math.cos(nouvel_angle)
-    #     ball.vel_y = ball.norm * math.sin(nouvel_angle)
+    # Gestion de la balle
+        # Rebond sur les bords de la fenêtre
+        if (ball.y - ball.radius + ball.vel_y) < 0 or (ball.y + ball.radius + ball.vel_y) > 840:
+            ball.angle *= -1
+            ball.vel_x = ball.norm * math.cos(ball.angle)
+            ball.vel_y = ball.norm * math.sin(ball.angle)
 
-    # Rebond sur les raquettes avec une réflexion physique
-    if design.verif_contact(J1, ball) and (ball.x - ball.radius + ball.vel_x) <= (J1.x + J1.width):
-        ball.vel_x = -ball.vel_x
+        # Rebond sur les raquettes avec distribution d'angles (ca marche po)
+        # if design.verif_contact(J1, ball) and (ball.x - ball.radius + ball.vel_x) <= (J1.x + J1.width):
+        #     nouvel_angle = design.distribution_reflexion(J1, ball, 55)
+        #     print(nouvel_angle, 'J1')
+        #     ball.vel_x = ball.norm*math.cos(nouvel_angle)
+        #     ball.vel_y = ball.norm*math.sin(nouvel_angle)
+        #
+        # if design.verif_contact(J2, ball) and (ball.x + ball.radius + ball.vel_x) >= J2.x:
+        #     nouvel_angle = design.distribution_reflexion(J2, ball, 55)
+        #     print(nouvel_angle, 'J2')
+        #     ball.vel_x = -ball.norm * math.cos(nouvel_angle)
+        #     ball.vel_y = ball.norm * math.sin(nouvel_angle)
 
-    if design.verif_contact(J2, ball) and (ball.x + ball.radius + ball.vel_x) >= J2.x:
-        ball.vel_x = -ball.vel_x
+        # Rebond sur les raquettes avec une réflexion physique (avec accelération)
+        if design.verif_contact(J1, ball) and (ball.x - ball.radius + ball.vel_x) <= (J1.x + J1.width):
+            ball.angle = math.pi - ball.angle
+            ball.norm += 1
+            ball.vel_x = ball.norm*math.cos(ball.angle)
+            ball.vel_y = ball.norm*math.sin(ball.angle)
 
-# Gestion des scores
-    if ball.x + ball.vel_x < 0:
-        pass
-    if ball.x + ball.vel_x > 1420:
-        pass
+        if design.verif_contact(J2, ball) and (ball.x + ball.radius + ball.vel_x) >= J2.x:
+            ball.angle = math.pi - ball.angle
+            ball.norm += 1
+            ball.vel_x = ball.norm * math.cos(ball.angle)
+            ball.vel_y = ball.norm * math.sin(ball.angle)
 
-    ball.x += ball.vel_x
-    ball.y += ball.vel_y
+    # Gestion des scores
+        if ball.x + ball.vel_x < 0:
+            scoreJ2 += 1
+            design.debut_manche(window, J1, J2, ball, scoreJ1, scoreJ2, initial_velocity)
 
-    design.update_window(window, J1, J2, ball)
-    cpt += 1
+        if ball.x + ball.vel_x > 1420:
+            scoreJ1 += 1
+            design.debut_manche(window, J1, J2, ball, scoreJ1, scoreJ2, initial_velocity)
+
+        ball.x += ball.vel_x
+        ball.y += ball.vel_y
+
+        design.update_window(window, J1, J2, ball, scoreJ1, scoreJ2)
+
 
 pg.quit()
